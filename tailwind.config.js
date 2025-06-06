10c9e5b0113e7856f7a051509aa86288f255e73f
maneltx <118104197+maneltx@users.noreply.github.com>
2025-06-06 19:37:06 +0200

    feat(app-router): migrate from pages to app router

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}'
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", 
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'q-indigo': '#12063e',
+        'q-lila': '#e0b3ff',
+        'q-violeta': '#1c124d',
+        'q-cian': '#75e6da',
+        'q-magenta': '#d46de7',
+        'q-dorado': '#d4af37',
+        'q-blanco-calido': '#f8f7f2',
+        'q-gris': '#a1a1aa',
+      },
+      fontFamily: {
+        'sans-clean': ['var(--font-inter)', 'sans-serif'],
+        'serif-elegant': ['var(--font-playfair)', 'serif'],
+      },
+    },
   },
   plugins: [],
 };
